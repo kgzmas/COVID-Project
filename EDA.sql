6ad5b168f82d931129e8a557b30798e2598257cf
Sam Zhu <97705838+kgzmas@users.noreply.github.com>
2024-10-14 16:11:48 -0700

    Update EDA.sql

--- EDA.sql
+++ EDA.sql
@@ -34,3 +34,65 @@ from PortfolioProject.dbo.coviddeaths
 where continent <> '' --This is super important as I just found out the dataset has rows with Continent or even World data.
 group by location, population
 order by highestdeaths desc
+
+-- Showing continent with the highest death count per population
+select continent, max(total_deaths) as totaldeath
+from PortfolioProject.dbo.coviddeaths
+where continent <> '' 
+group by continent
+order by totaldeath desc
+
+
+-- Global numbers
+select date, sum(cast(new_cases as float)) as TotalCase, sum(cast(new_deaths as float)) as TotalDeath, sum(cast(new_deaths as float)) / nullif(sum(cast(new_cases as float)),0)*100 as DeathPercentage
+from PortfolioProject.dbo.coviddeaths
+where continent <> ''
+group by date
+order by 1,2
+
+
+-- Total population vs. vaccinations
+
+-- Using CTE
+with PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingVaccinationCount)
+as  
+(select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
+sum(cast(vac.new_vaccinations as float)) over (partition by dea.location order by dea.location, dea.date) as RollingVaccinationCount
+from PortfolioProject.dbo.coviddeaths dea
+join PortfolioProject.dbo.covidvaccine vac
+on dea.location = vac.location and dea.date = vac.date
+where dea.continent <> ''
+)
+select *, (RollingVaccinationCount/Population)*100
+from PopvsVac
+
+
+-- Temp Table
+drop table if exists #PercentPopulationVaccinated
+create table #PercentPopulationVaccinated
+(Continent nvarchar(255), Location nvarchar(255), Date datetime, Population float, New_Vaccinations float, RollingVaccinationCount float)
+Insert into #PercentPopulationVaccinated
+select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
+sum(cast(vac.new_vaccinations as float)) over (partition by dea.location order by dea.location, dea.date) as RollingVaccinationCount
+from PortfolioProject.dbo.coviddeaths dea
+join PortfolioProject.dbo.covidvaccine vac
+on dea.location = vac.location and dea.date = vac.date
+where dea.continent <> ''
+
+
+-- Views for later visualizations
+create view PercentPopulationVaccinated as 
+select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
+sum(cast(vac.new_vaccinations as float)) over (partition by dea.location order by dea.location, dea.date) as RollingVaccinationCount
+from PortfolioProject.dbo.coviddeaths dea
+join PortfolioProject.dbo.covidvaccine vac
+on dea.location = vac.location and dea.date = vac.date
+where dea.continent <> ''
+
+create view DeathByContinent as
+Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+Where continent <> ''
+Group by continent
+
